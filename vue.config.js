29e3bb68f72cf75b49d4ef70e27dc3885e9e618e
Игорь Голанов <igor.golanoff@yandex.ru>
2022-04-10 17:27:03 +0300

    start commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: "warning",
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
 })
